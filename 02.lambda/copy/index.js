dbad7eec96203ec06bd0226ee59fadccdd8628bf
guzhongren <guzhongren@live.cn>
2020-05-15 13:38:58 +0800

    fix(lambda): updage lambda

--- 02.lambda/copy/index.js
+++ 02.lambda/copy/index.js
@@ -15,25 +15,24 @@ const s3 = new S3({ apiVersion: '2006-03-01' });
 
 exports.handler = async function (event) {
   console.log(JSON.stringify(event))
-  // const s3Event = event.Records[0].s3;
-  // var params = {
-  //   Bucket: "guzhongren-s3-bucket-target",
-  //   Key: `${s3Event.object.key}`,
-  //   CopySource: `${s3Event.bucket.name}/${s3Event.object.key}`,
-  // }
-  // console.log(JSON.stringify(params))
-  // const request = s3.copyObject(params, function (err, data) {
-  //   if (err) console.log(err, err.stack)
-  //   else console.log(data)
-  // });
-  // request.send();
+  const s3Event = event.Records[0].s3;
   var params = {
     "Bucket": "guzhongren-s3-bucket-target",
-    "Key": "guzhongren-s3-copy.zip",
-    "CopySource": "guzhongren-s3-bucket/guzhongren-s3-copy.zip",
+    "Key": `${s3Event.object.key}`,
+    "CopySource": `${s3Event.bucket.name}/${s3Event.object.key}`,
   }
-   await s3.copyObject(params, function (err, data) {
-    if (err) console.log(err, err.stack);
-    else console.log(data);
+  console.log(JSON.stringify(params))
+  const request = s3.copyObject(params, function (err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(data)
   }).promise()
+ // var params = {
+ //   "Bucket": "guzhongren-s3-bucket-target",
+ //   "Key": "guzhongren-s3-copy.zip",
+ //   "CopySource": "guzhongren-s3-bucket/guzhongren-s3-copy.zip",
+ // }
+ //  await s3.copyObject(params, function (err, data) {
+ //   if (err) console.log(err, err.stack);
+ //   else console.log(data);
+ // }).promise()
 }
